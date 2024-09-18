f9bf3347f7703156a26a8fba8daab2a36d1327ab
Andrew Consroe <aconz2@gmail.com>
2024-09-18 15:13:27 -0500

    create_v0

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 ```
+ls src/*.rs | entr -c bash -c 'cargo build && git ls-files | cargo run create_v0 /tmp/archive.v0'
 ls src/*.rs | entr -c bash -c 'cargo build && (cd ../linux; git ls-files | ../archive-testing/target/debug/archive-testing create_v0 /tmp/archive.v0)'
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
 use std::collections::HashSet;
 use std::path::Path;
-use std::io::stdin;
-use std::io::BufRead;
-// use std::fs::File;
+use std::io;
+use std::io::{stdin,BufRead,Write};
+use std::ffi::OsString;
+use std::os::unix::prelude::OsStrExt;
+use std::fs::File;
 
 
 #[derive(Debug)]
@@ -11,19 +13,31 @@ enum Error {
     NoOutfile,
 }
 
+fn join_bytes<'a, I: Iterator<Item = &'a [u8]>>(xs: I) -> Vec<u8> {
+    let mut acc: Vec<u8> = vec![];
+    for x in xs {
+        acc.extend_from_slice(x);
+        acc.push(0)
+    }
+    acc
+}
+
 /// v0 archive format
 /// num_dirs: u32le 
 /// num_files: u32le
 /// dirnames_size: u32le
 /// filenames_size: u32le
-/// <dirnames with null bytes>
-/// <filenames with null bytes>
-/// <num_files x u32le file sizes>
+/// <dirnames with null bytes> of length dirnames_size bytes
+/// <filenames with null bytes> of length filenames_size bytes
+/// <num_files x u32le file sizes> of length num_files * 4 bytes
 /// <data>
-
+/// ---
+/// input is line separated pathnames relative to cwd
+/// ---
+/// args <output file>
 fn create_v0(args: Vec<String>) {
-    let outname = args.get(1).ok_or(Error::NoOutfile).unwrap();
-    //let outfile = File::create(outname).unwrap();
+    let outname = args.get(2).ok_or(Error::NoOutfile).unwrap();
+    let mut outfile = File::create(outname).unwrap();
     let files = {
         let mut acc: Vec<_> = stdin().lock().lines().map(|x| x.unwrap()).collect();
         acc.sort();
@@ -32,25 +46,40 @@ fn create_v0(args: Vec<String>) {
     let mut size = 0;
     let dirs = {
         let mut acc = HashSet::new();
+        let empty = OsString::new();
         for file in &files {
             let p = Path::new(&file);
-            size += p.metadata().unwrap().len();
             if !p.is_file() { continue; }
+            size += p.metadata().unwrap().len();
             assert!(p.is_file(), "{:?} is not file", p);
             for parent in p.ancestors().skip(1) {
-                acc.insert(parent.to_owned());
+                if parent != empty {
+                    acc.insert(parent.to_owned());
+                }
             }
         }
         let mut acc: Vec<_> = acc.drain().collect();
         acc.sort();
         acc
     };
-    //for d in dirs {
-    //    println!("{:?}", d);
-    //}
+    let filesb = join_bytes(files.iter().map(|x| x.as_bytes()));
+    let dirsb = join_bytes(dirs.iter().map(|x| x.as_os_str().as_bytes()));
+    dbg!(&dirs);
     println!("there are {} dirs", dirs.len());
     println!("there are {} files", files.len());
     println!("total bytes of data {}", size);
+    println!("filenames len {}", filesb.len());
+    println!("dirsb len {}", dirsb.len());
+    println!("writing to {}", outname);
+    for i in vec![dirs.len(), files.len(), dirsb.len(), filesb.len()] {
+        outfile.write(&(i as u32).to_le_bytes()).unwrap();
+    }
+    outfile.write(&dirsb).unwrap();
+    outfile.write(&filesb).unwrap();
+    for file in &files {
+        let mut f = File::open(file).unwrap();
+        io::copy(&mut f, &mut outfile).unwrap();
+    }
 }
 
 fn main() {
